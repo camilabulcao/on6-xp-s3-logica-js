109fb5ff94798b24b66b725d736f7e690755b4ab
camilabulcao <camilabulcao5@gmail.com>
2020-08-27 16:56:40 -0300

    add atv 5

---
+++ Entrega/atv5_funcao.js
@@ -0,0 +1,14 @@
+//4. Crie uma função que receba um número e devolva uma soma progressiva. Por exemplo, recebendo o número 5, a função deve retornar 15, ou seja, o resultado de 1 + 2 + 3 + 4 + 5. 
+
+const query = require('synchronous-user-input');
+const num = parseInt(query('Digite um número: '));
+
+function somaProgressiva(num){
+    
+    let resultado = 0
+    for(i=1; i<=num; i++){  
+        resultado = resultado + i;
+    }
+    return resultado
+}
+console.log("A soma progressiva: " +somaProgressiva(num))
